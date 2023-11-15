333c7e94df620c126405a10c5864b1ad4e63f246
David Schloemer <david.schloemer@outlook.com>
2023-11-15 00:04:32 -0500

    Cards for Christmas List

--- src/app/christmaslist/christmaslist.component.ts
+++ src/app/christmaslist/christmaslist.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-christmaslist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './christmaslist.component.html',
   styleUrl: './christmaslist.component.css'
 })
 export class ChristmaslistComponent {
-
+  cardsData = [
+    {
+      title: 'Shiba Inu',
+      subtitle: 'Dog Breed',
+      imageSrc: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.'
+    },
+    {
+      title: 'Shiba Inu',
+      subtitle: 'Dog Breed',
+      imageSrc: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.'
+    },
+    // Add more card objects as needed
+  ];
 }
