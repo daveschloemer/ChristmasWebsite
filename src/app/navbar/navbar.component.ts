4ba95cf89b93ad3c8a9b0785baa19b1c89092169
David Schloemer <david.schloemer@outlook.com>
2023-11-16 13:04:40 -0500

    Updated Nav Bar for Desktop and Mobile Users

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatDividerModule, MatMenuModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatDividerModule, MatMenuModule, RouterOutlet, RouterLink, FlexLayoutModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'] // Fixed typo: changed `styleUrl` to `styleUrls`
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isSmallScreen = false;
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.Small, Breakpoints.XSmall,Breakpoints.Medium]).subscribe(result => {
+      this.isSmallScreen = result.matches;
+    });
+  }
 }
